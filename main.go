404ab4fa48b3e05d1cc2aa27aa1a9ee95e3a9d8d
snarksliveshere <snarksliveshere@gmail.com>
2020-01-19 20:33:27 +0300

    get simple formula of algorithm

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 type Banner struct {
+	Id     int
 	Trials int
 	Reward int
 }
@@ -11,5 +17,30 @@ type Banners struct {
 }
 
 func main() {
+	bs := []Banner{
+		{
+			Id:     1,
+			Trials: 20,
+			Reward: 2,
+		},
+		{
+			Id:     2,
+			Trials: 30,
+			Reward: 4,
+		},
+		{
+			Id:     3,
+			Trials: 20,
+			Reward: 6,
+		},
+	}
+	banners := Banners{Count: 70, Banners: bs}
+	for _, v := range banners.Banners {
+		profit := float64(v.Reward) / float64(v.Trials)
+		rs := profit + math.Sqrt(math.Log(float64(banners.Count))/float64(v.Trials))
+		fmt.Println(rs, v.Id)
 
+	}
 }
+
+//val[i] = x_mean[i] + math.Sqrt(math.Log(float64(agent.Trials))/(2*float64(arm[i].Count)))
